2a33e6258b1bfc185b46153e727341ea00fcb79f
J Carlos Viana F <jcarlosvianaf.dev@gmail.com>
2022-05-18 14:38:29 -0300

    Revert "Update item.py"
    This reverts commit c55f9224c87d48c272f3d1c9400292d859b0123f.

--- magparser/item.py
+++ magparser/item.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-import requests
 import datetime
 
 class Item:
@@ -24,24 +23,5 @@ class Item:
             self.date = datetime.datetime.strptime(self.date[date_index:date_index+11].strip(), \
                 "%d %b %Y").date()
         
-        #self.image = base + html.find('div', {'class': 'list_box_lit'}).find('img')['src']
-        self.image = self.get_google_img(self.title)
+        self.image = base + html.find('div', {'class': 'list_box_lit'}).find('img')['src']
         self.link = base + html.find('div', {'class': 'list_box_title'}).find('a')['href']
-
-    def get_google_img(self, query):
-        """
-        gets a link to the first google image search result
-        :param query: search query string
-        :result: url string to first result
-        """
-        url = "https://www.google.com/search?q=" + str(query) + "&source=lnms&tbm=isch"
-        headers={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-
-        html = requests.get(url, headers=headers).text
-
-        soup = BeautifulSoup(html, 'html.parser')
-        image = soup.find("img",{"class":"t0fcAb"})
-
-        if not image:
-            return 
-        return image['src']
