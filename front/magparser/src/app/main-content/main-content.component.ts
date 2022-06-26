ced990dd4c227f3ee67f96d78171d0b080e21800
J Carlos Viana F <jcarlosvianaf.dev@gmail.com>
2022-06-26 14:38:55 -0300

    listen page change

--- front/magparser/src/app/main-content/main-content.component.ts
+++ front/magparser/src/app/main-content/main-content.component.ts
@@ -1,29 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Magazine } from '../content/magazine';
+import { MagazineService } from './magazine-service.service';
 
 @Component({
   selector: 'page-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
-export class MainContentComponent implements OnInit {
-
+export class MainContentComponent implements OnChanges {
   @Input() selectedTopic:string = "";
   @Input() startDate:Date|null = null;
   @Input() endDate:Date|null = null;
   @Input() searchTerm:string|null = null;
 
-  magazines:Magazine[];
-  currentPage:Number = 1;
+  magazines:Magazine[] = [];
+  currentPage:number = 1;
   hasNextPage:Boolean = false;
   maxContentPerPage:Number = 10;
 
-  constructor() {
-    // run service to get mags
-    this.magazines = [new Magazine("mag1"), new Magazine("mag2")];
+  constructor(private magazineService:MagazineService) {
+    this.getMags();
   }
 
-  ngOnInit(): void {
+  private createSearchObj(){
+    return {
+      selectedTopic: this.selectedTopic,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      searchTerm: this.searchTerm,
+      currentPage: this.currentPage,
+      maxContentPerPage: this.maxContentPerPage
+    };
   }
 
+  getMags(event:any = null){
+    if (event != null){
+      this.currentPage = event.currentPage;
+    }
+
+    var resultObj = this.magazineService.getMags(this.createSearchObj());
+    this.magazines = resultObj.mags;
+    this.hasNextPage = resultObj.hasNextPage;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('selectedTopic' in changes || 'startDate' in changes ||
+        'endDate' in changes || 'searchTerm' in changes){
+      this.currentPage = 1;
+      this.getMags();
+    }
+  }
 }
