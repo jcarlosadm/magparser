481b131e54b394ee440b8a015688f7b48431576c
J Carlos Viana F <jcarlosvianaf.dev@gmail.com>
2022-06-27 20:02:16 -0300

    add topics getter on backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+from datetime import date, timedelta, datetime
+from topics import get_topics
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -8,7 +12,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/api/get_topics")
 @cross_origin()
 def getTopics():
-    return jsonify(["All", "Computer", "Science"])
+    start_date = request.args.get('startDate', default = date.today() - timedelta(days=5), \
+        type = to_date).strftime("%Y-%m-%d")
+    end_date = request.args.get('endDate', default = date.today(), type = to_date)\
+        .strftime("%Y-%m-%d")
+
+    # TODO: use cache
+    return jsonify(get_topics(start_date, end_date))
+
+def to_date(dateString):
+    return datetime.strptime(dateString, "%Y-%m-%d").date()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
---
+++ backend/topics.py
@@ -0,0 +1,65 @@
+from bs4 import BeautifulSoup
+import requests
+
+DEFAULT_URL = "http://www.ebook3000.com/index_%s.htm"
+
+def get_topics(start_date, end_date):
+    stop_search = False
+    current_page = 1
+    topics = ["All"]
+    while(not stop_search):
+        page_url = DEFAULT_URL % current_page
+
+        headers = requests.utils.default_headers()
+        headers.update({ 'User-Agent': 'Mozilla/5.0 \
+            (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+        
+        req = requests.get(page_url, headers)
+
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        html_items = soup.find_all("div", {'class':'index_box'})
+
+        for item in html_items:
+            date = extract_date(item)
+            
+            if (date >= start_date and date <= end_date):
+                topic = extract_topic(item)
+                if topic not in topics:
+                    topics.append(topic)
+            else:
+                stop_search = True
+
+        current_page += 1
+
+    return topics
+
+def extract_date(html_item):
+    txt = html_item.find('div', {'class': 'index_box_tools'}).find_all(text=True)[2]
+
+    return format_date(txt[len('Date:')+1:-2])
+
+def extract_topic(html_item):
+    txt = html_item.find('div', {'class': 'index_box_tools'})\
+        .find('a').find_all(text=True)
+    return txt
+
+def format_date(date):
+    date_parts = date.split(' ')
+    return date_parts[2] + "-" + get_month(date_parts[1]) + "-" + date_parts[0]
+
+def get_month(m_str):
+    return {
+        'Jan':'01',
+        'Feb':'02',
+        'Mar':'03',
+        'Apr':'04',
+        'May':'05',
+        'Jun':'06',
+        'Jul':'07',
+        'Aug':'08',
+        'Sep':'09',
+        'Oct':'10',
+        'Nov':'11',
+        'Dec':'12'
+    }.get(m_str)
