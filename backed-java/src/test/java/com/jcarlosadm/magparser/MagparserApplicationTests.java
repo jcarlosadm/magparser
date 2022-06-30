de506e50c33404cca320cbd1b93fa5b464ac03f7
J Carlos Viana F <jcarlosvianaf.dev@gmail.com>
2022-06-30 12:45:56 -0300

    add backend-java project

---
+++ backed-java/src/test/java/com/jcarlosadm/magparser/MagparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarlosadm.magparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
